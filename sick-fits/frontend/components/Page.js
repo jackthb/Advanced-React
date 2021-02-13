07559c6ec1b02fc4dd05f5fb74b2722e8f7672dd
jackthb <19895932+jackthb@users.noreply.github.com>
2021-02-13 15:28:34 +0000

    yaowwwww

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,63 @@
 import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
-export default function Page({ children, cool }) {
+// HTML or :root -> :root can be used in standalone SVGs
+
+const GlobalStyes = createGlobalStyle`
+@font-face {
+  font-family: 'radnika_next';
+  src: url('/static/radnikanext-medium-webfont.woff2')
+  format('woff2');
+  font-weight:normal;
+  font-size:normal;
+}
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3a3a3a;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+    box-sizing: border-box;
+
+  }
+  *, *:before, *:after{
+    box-sizing: inherit;
+  }
+  body{
+    font-family: 'radnika_next', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1.5em;
+    padding: 0;
+    margin: 0;
+    line-height: 2;
+  }
+  a{
+    text-decoration:none;
+    color: var(---black);
+  }
+  a:hover{
+    text-decoration:underline;
+  }
+  buton {
+    font-family: 'radnika_next', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
+export default function Page({ children }) {
   return (
     <div>
+      <GlobalStyes />
       <Header />
-      <h2>I AM PAGE</h2>
-      <h2>HEHE</h2>
-      {children}
-      {cool}
-      <code>BRUH</code>
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 }
